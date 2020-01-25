993056120981146fb810fdf0ea7dfadecd23fba4
Barbara Mayfield <bmayfield1991@gmail.com>
2020-01-24 18:40:43 -0600

    Added config/secret.js, set up auth login/register routes, users-model and users-router, installed bcrypt and jsonwebtoken

---
+++ users/users-router.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const usersModel = require("./users-model")
+
+const router = express.Router()
+
+router.get("/", async (req, res, next) => {
+    try {
+        const users = await usersModel.find()
+
+        res.json(users)
+    } catch(err) {
+        next(err)
+    }
+})
